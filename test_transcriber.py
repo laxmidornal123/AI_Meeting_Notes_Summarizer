d9dcbfd3736244cb59509bd93bcabb6c3013b00f
Laxmi Dornal <laxmidornal12@gmail.com>
2026-06-29 23:22:22 +0530

    Complete project update

--- test_transcriber.py
+++
@@ -1,8 +0,0 @@
-from utils.transcribe import transcribe_audio
-
-text = transcribe_audio("audio/OSR_us_000_0039_8k.wav")
-
-print("\n====================")
-print("TRANSCRIPT")
-print("====================")
-print(text)
